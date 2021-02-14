c71a348b9f3fa4839cbbcc7b7929a3b4aa96769c
Jim Berry <jimalexanderberry@gmail.com>
2021-02-14 21:16:43 +0000

    App commit

---
+++ psych/src/App.tsx
@@ -0,0 +1,68 @@
+import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import db from './firebase';
+import './App.css';
+import Login from './pages/Login';
+import { Switch, Route } from 'react-router';
+import LandingPage from './pages/LandingPage';
+import GameLobby from './pages/GameLobby';
+import HostGameScreen from './pages/HostGameScreen';
+import GamePage from './pages/GamePage';
+import { AnimatePresence } from 'framer-motion';
+import TestPage from './pages/TestPage';
+
+function App() {
+  const [user, setUser] = useState(null);
+  const [gameCode, setGameCode]: any = useState('');
+  const [errorOrInfoText, setErrorOrInfoText] = useState('');
+
+  let AppPage = (
+    <Login
+      changeUser={(value: any) => setUser(value)}
+      changeGameCode={(userGameCode: string) => setGameCode(userGameCode)}
+    />
+  );
+  if (user) {
+    AppPage = (
+      <AnimatePresence exitBeforeEnter>
+        <Switch>
+          <Route path="/lobby">
+            <GameLobby gameCode={gameCode} user={user} />
+          </Route>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <LandingPage
+                gameCodeChanged={(value: any) => setGameCode(value)}
+                gameCode={gameCode}
+                user={user}
+                handleError={(text: string) => setErrorOrInfoText(text)}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/hostgame"
+            render={() => (
+              <HostGameScreen
+                gameCode={gameCode}
+                user={user}
+                setGameCode={setGameCode}
+              />
+            )}
+          />
+          <Route exact path="/hostgame">
+            <TestPage />
+          </Route>
+          <Route exact path="/game">
+            <GamePage gameCode={gameCode} user={user} />
+          </Route>
+        </Switch>
+      </AnimatePresence>
+    );
+  }
+  return <div>{AppPage}</div>;
+}
+
+export default App;
