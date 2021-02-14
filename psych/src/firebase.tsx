04319af7ac0545ae3e938d4c040b80da1c6475e1
Jim Berry <jimalexanderberry@gmail.com>
2021-02-14 21:16:49 +0000

    firebase commit

---
+++ psych/src/firebase.tsx
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyAg3F-mJFhWb11EQBsP8kftKDzGAlW6gMo',
+  authDomain: 'psych2.firebaseapp.com',
+  projectId: 'psych2',
+  storageBucket: 'psych2.appspot.com',
+  messagingSenderId: '144426654499',
+  appId: '1:144426654499:web:628b25489a670a26a74a96',
+  measurementId: 'G-8M0FJD320X',
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, firebaseApp };
+export default db;
