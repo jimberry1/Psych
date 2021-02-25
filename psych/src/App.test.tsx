e52dc5e9de2db66001b89445bf3dac175724a98e
Jim Berry <jimalexanderberry@gmail.com>
2021-02-25 20:36:13 +0000

    Adding plan for improvements

--- psych/src/App.test.tsx
+++ psych/src/App.test.tsx
@@ -7,3 +7,30 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+/**
+ * MY PLAN
+ *
+ * Goals
+ *
+ *  - Improve the landing page, make it farrrr more user friendly. That means don't force people to log in immedaitely and instead introduce the game and what it is. Give examples, screenshots
+ * and generally make this as approachable as it possibly can be
+ *
+ * - Add mixed games, whereby you can host games with a mix of questions from both the standard library and other libraries
+ *
+ * - Make it so that the host can use the 'rejoin' functionality
+ *
+ * - Improve account features, present an area to change the user's profile picture and don't forget to log the user out when they update their profile picture.
+ *
+ * - Record user scores and top answers. Create a leaderboard
+ *
+ * - Create a game over screen that occurs when the round has reached the maximum number of rounds. Offer a questions in review stage for this where the users can look back over all answers
+ *
+ * - Come up with a better name
+ *
+ * - Create pages for the privacy policy and maybe add a footer to the application as well.
+ *
+ * - Improve the add question screen to make it clearer how to link to
+ *
+ *
+ */
